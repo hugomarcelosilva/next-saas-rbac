d69783144e5a931e82ef477320494418f8e94b0b
Hugo Silva <hugo.marcelo@clevertech.biz>
2024-04-26 10:44:31 -0300

    feat(api): add get profile and authenticate routes

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -5,6 +5,8 @@ import { z } from 'zod'
 
 import { prisma } from '@/lib/prisma'
 
+import { BadRequestError } from '../_errors/bad-request-error'
+
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
@@ -27,9 +29,7 @@ export async function createAccount(app: FastifyInstance) {
       })
 
       if (userWithSameEmail) {
-        return reply.status(409).send({
-          message: 'User with this email already exists.',
-        })
+        throw new BadRequestError('User with this email already exists.')
       }
 
       const [, domain] = email.split('@')
